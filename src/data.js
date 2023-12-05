8b41d403751f34483b45089eea5b98a0477bb07e
abdullah4887 <100029017+abdullah4887@users.noreply.github.com>
2023-12-05 11:46:12 +0800

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,6 +2,7 @@ const reviews = [
   {
     id: 1,
     name: 'susan smith',
+    age: 22,
     job: 'web developer',
     image: 'https://course-api.com/images/people/person-1.jpeg',
     text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
